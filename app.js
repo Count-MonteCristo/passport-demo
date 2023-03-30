c89276fd4a3a71fb98f44663021c473318426732
Count-MonteCristo <luis0425navarro@gmail.com>
2023-03-29 20:11:36 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,120 @@
+require("dotenv").config();
+var bcrypt = require("bcryptjs");
+
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//connect to database
+const mongoDb = process.env.MONGO_URI;
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+//user model
+const User = mongoose.model(
+  "User",
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+  })
+);
+
+//set the view engine to ejs
+const app = express();
+app.set("views", __dirname);
+app.set("view engine", "ejs");
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+//Passport functions
+//Function one : setting up the LocalStrategy
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
+      // return done(null, user);
+    });
+  })
+);
+//Functions two and three: Sessions and serialization
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(async function (id, done) {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+//middleware
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+//routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+app.get("/log-out", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+//controllers
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    await User.create({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
